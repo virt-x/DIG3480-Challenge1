593ac3cfa4817fa0e4449efba3ce2a48887f75fc
virt-x <xvirt@outlook.com>
2020-01-31 08:40:45 -0500

    ufo 2.0

---
+++ Tutorial 1/Assets/Scripts/PickupController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CompareTag("Pickup"))
+        {
+            transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, -160) * Time.deltaTime);
+        }
+    }
+}
---
+++ Tutorial 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D player;
+    public float speed;
+    private int count, score, lives;
+    public UnityEngine.UI.Text scoreUI,winUI,lifeUI;
+    private bool win;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        score = 0;
+        lives = 3;
+        win = false;
+        winUI.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        player.AddForce(new Vector2(moveHorizontal * speed, moveVertical * speed));
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Pickup"))
+        {
+            collider.gameObject.SetActive(false);
+            count++;
+            score++;
+            scoreUI.text = "SCORE: " + score.ToString();
+        }
+
+        if (collider.gameObject.CompareTag("Enemy") && !win)
+        {
+            collider.gameObject.SetActive(false);
+            score--;
+            lives--;
+            scoreUI.text = "SCORE: " + score.ToString();
+            lifeUI.text = "LIVES: " + lives.ToString();
+        }
+
+        if (count == 12)
+        {
+            gameObject.transform.position = new Vector3(75, 0, 0);
+        }
+
+        if (count == 20)
+        {
+            win = true;
+            winUI.text = "Congratulations! You win!\n";
+            if (score == 20)
+            {
+                winUI.text += "-PERFECT SCORE-";
+            }
+            else
+            {
+                winUI.text += "Game created by Xavier Virt.";
+            }
+        }
+
+        if (lives == 0)
+        {
+            winUI.text = "You LOSE!\n-GAME OVER-";
+            gameObject.SetActive(false);
+        }
+    }
+}
